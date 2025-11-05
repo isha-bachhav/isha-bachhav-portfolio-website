52f41c96c0ef7fdf9abf5b665a240647c3ba2abe
ISHA PRASHANT BACHHAV <ishabachhav2005@gmail.com>
2025-11-05 11:32:39 +0530

    Update portfolio

--- isha-portfolio/vite.config.ts
+++ isha-portfolio/vite.config.ts
@@ -6,6 +6,7 @@ import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/isha-bachhav-portfolio-website/", // Important for GitHub Pages
   plugins: [react()],
   css: {
     postcss: {
